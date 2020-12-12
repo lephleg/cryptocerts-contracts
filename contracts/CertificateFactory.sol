5c239445cbe8b74549545fa2386d8e6c5eb36887
lephleg <lephleg@gmail.com>
2020-12-12 22:32:33 +0200

    style: auto-indentation fixes

--- contracts/CertificateFactory.sol
+++ contracts/CertificateFactory.sol
@@ -5,30 +5,45 @@ pragma solidity ^0.7.0;
 import "./InstitutionFactory.sol";
 
 contract CertificateFactory is InstitutionFactory {
-    
-    event CertificateCreated(uint indexed id, bytes32 digest, address indexed addr);
+    event CertificateCreated(
+        uint256 indexed id,
+        bytes32 digest,
+        address indexed addr
+    );
 
     // uint8 defaultHashFunction = 12; // sha-2
     // uint8 defaultDigestSize = 20; // 256-bits
 
     struct Certificate {
-      string title;
-      bytes32 digest;
-      uint8 hashFunction;
-      uint8 size;
-      uint createdAt;
+        string title;
+        bytes32 digest;
+        uint8 hashFunction;
+        uint8 size;
+        uint256 createdAt;
     }
-    
-    mapping (address => uint[]) public studentToCertificates;
-    mapping (address => uint[]) public institutionToCertificates;
+
+    mapping(address => uint256[]) public studentToCertificates;
+    mapping(address => uint256[]) public institutionToCertificates;
 
     Certificate[] public certificates;
-    
-    function createCertificate(string memory _title, bytes32 _digest, uint8 _hashFunction, uint8 _size, address _address) public {
-        Certificate memory cert = Certificate(_title, _digest, _hashFunction, _size, block.timestamp);
+
+    function createCertificate(
+        string memory _title,
+        bytes32 _digest,
+        uint8 _hashFunction,
+        uint8 _size,
+        address _address
+    ) public {
+        Certificate memory cert = Certificate(
+            _title,
+            _digest,
+            _hashFunction,
+            _size,
+            block.timestamp
+        );
         certificates.push(cert);
 
-        uint id = certificates.length - 1;
+        uint256 id = certificates.length - 1;
         studentToCertificates[_address].push(id);
         institutionToCertificates[_msgSender()].push(id);
 
--- contracts/InstitutionFactory.sol
+++ contracts/InstitutionFactory.sol
@@ -5,49 +5,61 @@ pragma solidity ^0.7.0;
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract InstitutionFactory is Ownable {
-    
-    event InstitutionCreated(uint indexed id, string name, address indexed addr);
-    
+    event InstitutionCreated(
+        uint256 indexed id,
+        string name,
+        address indexed addr
+    );
+
     struct Institution {
         string name;
         bool isValid;
     }
-    
-    mapping (address => uint) public ownerToInstitution;
-    mapping (uint => address) public institutionToOwner;
-    
+
+    mapping(address => uint256) public ownerToInstitution;
+    mapping(uint256 => address) public institutionToOwner;
+
     Institution[] public institutions;
 
     /**
      * @dev Throws if called by any account who doesn't beloong to an Institution.
      */
     modifier onlyInstitution() {
-        require(ownerToInstitution[_msgSender()] != 0, "Caller is not an institution owner");
+        require(
+            ownerToInstitution[_msgSender()] != 0,
+            "Caller is not an institution owner"
+        );
         _;
     }
-    
-    function createInstitution(string memory _name, address _address) public onlyOwner {
+
+    function createInstitution(string memory _name, address _address)
+        public
+        onlyOwner
+    {
         institutions.push(Institution(_name, true));
 
-        uint id = institutions.length - 1;
+        uint256 id = institutions.length - 1;
         ownerToInstitution[_address] = id;
         institutionToOwner[id] = _address;
-        
+
         InstitutionCreated(id, _name, _address);
     }
-    
-    function editInstitution(uint _id, string memory _name) public onlyOwner {
+
+    function editInstitution(uint256 _id, string memory _name)
+        public
+        onlyOwner
+    {
         institutions[_id].name = _name;
     }
-    
-    function deleteInstitution(uint _id) public onlyOwner {
+
+    function deleteInstitution(uint256 _id) public onlyOwner {
         Institution memory institution = institutions[_id];
         institution.isValid = false;
         ownerToInstitution[institutionToOwner[_id]] = 0;
         institutionToOwner[_id] = address(0);
     }
 
-    function getInstitutionsSize() external view returns (uint) {
+    function getInstitutionsSize() external view returns (uint256) {
         return institutions.length;
     }
 }
